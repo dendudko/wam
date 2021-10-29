4c285c5d6568a91a10028d48a49b12868e5c1ec0
dendudko <dudko.do@students.dvfu.ru>
2021-10-30 01:53:42 +1000

    Цвета кнопки

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ let ep = 1;
 
 function chooseMole(){
     if (speed==1){
-        document.getElementById('KK').style.color="red";
+        document.getElementById('KK').style.color="darkred";
         document.getElementById('KA').style.color = "#ffcd08";
         document.getElementById('KT').style.color = "#ffcd08";
     }
     else if (speed==2) {
         document.getElementById('KK').style.color = "#ffcd08";
-        document.getElementById('KA').style.color = "red";
+        document.getElementById('KA').style.color = "darkred";
         document.getElementById('KT').style.color = "#ffcd08";
     }
     else if (speed==3) {
         document.getElementById('KK').style.color = "#ffcd08";
         document.getElementById('KA').style.color = "#ffcd08";
-        document.getElementById('KT').style.color = "red";
+        document.getElementById('KT').style.color = "darkred";
     }
 }
 
